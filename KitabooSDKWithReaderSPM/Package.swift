7c902bbca341c288309dcb4a077e29aa158fe068
hardikkumar.hadwani <hardikkumar.hadwani@hurix.com>
2023-03-04 00:50:20 +0530

    Added reader sdk dependencies

--- KitabooSDKWithReaderSPM/Package.swift
+++ KitabooSDKWithReaderSPM/Package.swift
@@ -14,17 +14,20 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/hardikhadwani-hurix/MyFirstSPM", from: "1.2.0"),
     ],
-    targets: [
+     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "KitabooSDKWithReader", url: "https://github.com/hardikhadwani-hurix/KitabooReaderSDK/releases/download/1.0/Kitaboo_Reader_SDK.xcframework.zip", checksum: "528830e2c0b38ef72fe8e1a8c1575a87d30d6681b6f0dba00e52f97af3d410bc"),
+        .binaryTarget(name: "KitabooSDKWithReader", url: "https://github.com/hardikhadwani-hurix/KitabooSDKWithReader/releases/download/1.0.0/KitabooSDKWithReader.xcframework.zip", checksum: "528830e2c0b38ef72fe8e1a8c1575a87d30d6681b6f0dba00e52f97af3d410bc"),
         .target(
             name: "KitabooSDKWithReaderSPM",
-            dependencies: []),
+            dependencies: [
+                .product(name: "HardikSPM", package: "MyFirstSPM"),
+            ]),
         .testTarget(
             name: "KitabooSDKWithReaderTests",
             dependencies: ["KitabooSDKWithReaderSPM"]),
     ]
+        
 )
