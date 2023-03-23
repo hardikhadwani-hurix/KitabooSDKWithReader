1218845dab768349416de62bf3f2ff54ebfa6bdd
hardikkumar.hadwani <hardikkumar.hadwani@hurix.com>
2023-03-23 14:26:49 +0530

    Resolved issue related 'sharedApplication' is unavailable: not available on iOS (App Extension)

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
      targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "KitabooSDKWithReader", url: "https://github.com/hardikhadwani-hurix/KitabooSDKWithReader/releases/download/1.0.0/KitabooSDKWithReader.xcframework.zip", checksum: "528830e2c0b38ef72fe8e1a8c1575a87d30d6681b6f0dba00e52f97af3d410bc"),
+        .binaryTarget(name: "KitabooSDKWithReader", url: "https://github.com/hardikhadwani-hurix/KitabooSDKWithReader/releases/download/1.0.0/KitabooSDKWithReader.xcframework.zip", checksum: "58dc4444d27d0a0d666f82fa9a4c30457a54c4b9c5b47de57b5816a316a8e605"),
         .target(
             name: "KitabooSDKWithReaderSPM",
             dependencies: [
