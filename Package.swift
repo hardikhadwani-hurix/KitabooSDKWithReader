5b824a36bb158a149570e93e183ae9e27dfea89f
hardikkumar.hadwani <hardikkumar.hadwani@hurix.com>
2023-04-06 16:54:13 +0530

    Updated framework for crash issue

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
      targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "KitabooSDKWithReader", url: "https://github.com/hardikhadwani-hurix/KitabooSDKWithReader/releases/download/1.0.6/KitabooSDKWithReader.xcframework.zip", checksum: "4f4a65e71ead29ff3c801d4213c3613831fb028a9252d5a498dbd6b9add35d02"),
+        .binaryTarget(name: "KitabooSDKWithReader", url: "https://github.com/hardikhadwani-hurix/KitabooSDKWithReader/releases/download/1.0.8/KitabooSDKWithReader.xcframework.zip", checksum: "4f4a65e71ead29ff3c801d4213c3613831fb028a9252d5a498dbd6b9add35d02"),
         .target(
             name: "KitabooSDKWithReaderSPM",
             dependencies: [
